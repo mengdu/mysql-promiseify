cec7d2bcf8b7fe603a0f13efa5eff6d5a493a387
jane <lanyueos@qq.com>
2022-10-27 12:20:17 +0800

    fix: 修改声明文件

--- index.d.ts
+++ index.d.ts
@@ -9,5 +9,5 @@ declare class PoolPormiseify {
   begin <T extends any>(fn: (c: Omit<PoolPormiseify, 'begin'>, util: { rollback: () => Promise<void> }) => Promise<T>): Promise<T>
 }
 
-export default function createPool (config: PoolConfig | string, logger?: (sql: string, ts: number, res: any) => void): PoolPormiseify
-export {}
+declare function createPool (config: PoolConfig | string, logger?: (sql: string, ts: number, res: any) => void): PoolPormiseify
+export = createPool
